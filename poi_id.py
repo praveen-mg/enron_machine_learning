40030733fa20067d9ce5eae1417a64b1a0e655ca
Praveen Kumar Meenakshi Sundaram Gandhi <praveen_mg17@yahoo.co.in>
2016-08-28 22:55:13 +0800

    remove outlier

--- poi_id.py
+++ poi_id.py
@@ -17,159 +17,196 @@ def removekey(d, key):
     r = dict(d)
     del r[key]
     return r
-### Task 1: Select what features you'll use.
-### features_list is a list of strings, each of which is a feature name.
-### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
-features = ['salary','bonus']
-### Load the dictionary containing the dataset
-with open("final_project_dataset.pkl", "r") as data_file:
-    data_dict = pickle.load(data_file)
+    
+def float_to_int(val):
+    if type(val) is str:
+        return val
+    else:
+        if pd.isnull(val):
+            return val
+        elif type(val) is float:
+            return int(val)
+        else:
+            return val
+    
+def convert_dict(data_frame):
+    
+    #temp_df = data_frame.astype(int,errors='coerce')
+    print data_frame.head()
+    temp_df = data_frame
+    #for col in temp_df.columns.values:
+        #print col
+        #temp_df[col] = temp_df[col].map(float_to_int)
+#        for val in data_frame[col]:
+#            if not pd.isnull(val):
+    
+    #temp_df = data_frame.apply(float_to_int)
+    #temp_df = temp_df.applymap(float_to_int)
+    print "\n\n\nTEMP DF"
+    print temp_df.head()
+    temp_df = data_frame.fillna('NaN')
+    temp_df = data_frame.transpose()
+    dataset = temp_df.to_dict()
+#    for key in dataset:
+#        for key1,val in dataset[key].iteritems():
+#            dataset[key][key1] =  float_to_int(val)
+   
+    return dataset
+    
+if __name__ == "__main__":
+    
+    ### Task 1: Select what features you'll use.
+    ### features_list is a list of strings, each of which is a feature name.
+    ### The first feature must be "poi".
 
-#print data_dict.keys()
-
-#Get total number of features and data points
-person_data = pd.DataFrame(data_dict)   
-person_data = person_data.transpose()
-
-
-print "Total Number of Features", person_data.shape[1]
-print "Total Number of DataPoints", person_data.shape[0]
-
-#print person_data.head
-#Clean the data replace string nan with numpy Nan
-
-
-
-
-
-
-#person_data = pd.to_numeric(person_data,args=('coerce',))
-person_data = person_data.apply(pd.to_numeric, errors='coerce')
-person_data['email_address'] = person_data['email_address'].astype(str)
-
-print "Data type of data frame", person_data.dtypes
-
-#for val in list(person_data.columns.values):
-    #if val != 'email_address':
+    features_list = ['poi','salary'] # You will need to use more features
+    features = ['salary','bonus']
+    
+    ### Load the dictionary containing the dataset
+    
+    with open("final_project_dataset.pkl", "r") as data_file:
+        data_dict = pickle.load(data_file)
         
+    #Convert data into dataframe for easy processing
+    #Get total number of features and data points
+    
+    person_data = pd.DataFrame(data_dict)   
+    person_data = person_data.transpose()
+    print "Total Number of Features", person_data.shape[1]
+    print "Total Number of DataPoints", person_data.shape[0]
+    
+     #Writing data to CSV
+    
+    person_data.to_csv('data.csv')
+    
+    print "\nCSV File Created"
+    #Clean the data replace string nan with numpy Nan
+    
+    person_data = person_data.apply(pd.to_numeric, errors='coerce')
+    person_data['email_address'] = person_data['email_address'].astype(str)
+    print "Data type of data frame", person_data.dtypes
+    print "Nan data in column\n",person_data.isnull().sum()
+    poi_count = person_data['poi'].loc[person_data['poi'] == True].count()
+    print "Number of POI",person_data['poi'].loc[person_data['poi'] == True].count()
+    print "Number of non POI", (person_data.shape[0] - poi_count)
+    print person_data.columns.values
 
-
-print "Nan data in column\n",person_data.isnull().sum()
-
-
-poi_count = person_data['poi'].loc[person_data['poi'] == True].count()
-print "Number of POI",person_data['poi'].loc[person_data['poi'] == True].count()
-
-print "Number of non POI", (person_data.shape[0] - poi_count)
-
-print person_data.columns.values
-#for data in data_dict:
+        
+    ### Task 2: Remove outliers
+  
+    """
+    for point in data:
+        salary = point[0]
+        bonus = point[1]
+        #if bonus != 97343619.0:
+        if bonus != max_bonus:
+            matplotlib.pyplot.scatter( salary, bonus )
+            
+    """
+    #Analysis Based on Plotting
+    
+    
+    
+    matplotlib.pyplot.scatter( person_data['salary'], person_data['bonus'])
+    matplotlib.pyplot.xlabel("salary")
+    matplotlib.pyplot.ylabel("bonus")
+    matplotlib.pyplot.title("Before Removing the outlier")
+    matplotlib.pyplot.show() 
+    
+    #Find Outlier and remove it
+          
+    max_bonus = person_data['bonus'].max()
+    data = featureFormat(data_dict, features)
+    print "Maximum bonus",max_bonus
+    name_outlier = person_data[person_data['bonus'] == max_bonus].index.tolist()[0]
+    print "Person With Max bonus:",name_outlier
+    print "Details of the person with max bonus", data_dict[name_outlier]
+    print "Total rows:",len(data_dict)
+    #my_dataset = removekey(data_dict,name_outlier)
+    data_dict  = removekey(data_dict,name_outlier)
+    my_dataset = data_dict
+    person_data = person_data[person_data['bonus'] < max_bonus]
+    my_dataset1 = convert_dict(person_data)
+    #print "Return Value:%d"%  cmp (my_dataset, my_dataset1)
+    matplotlib.pyplot.scatter( person_data['salary'], person_data['bonus'],c=person_data['poi'])
+    matplotlib.pyplot.xlabel("salary")
+    matplotlib.pyplot.ylabel("bonus")
+    matplotlib.pyplot.title("After Removing the outlier")
+    matplotlib.pyplot.show() 
+    
+    #Check For other Outliers
+    
+    other_outlier = person_data[(person_data['bonus'] > 5000000) & (person_data['salary'] > 1000000)]
+    print other_outlier
+    print "my dataset:",len(my_dataset)
+    print "my dataset1:",len(my_dataset1)
+    for key in my_dataset:
+        if key in my_dataset1:
+            continue
+        else:
+            print "Key is miss",key
+#        for key1 in my_dataset[key]:
+#            if my_dataset[key][key1] == my_dataset1[key][key1]:
+#                continue
+#            else:
+#                print "key",key
+#                print "key1",key1
+#                print "Val",my_dataset[key][key1]
+#                print "Val1",my_dataset1[key][key1]
+#            if val == val1:
+#                continue
+#            else:
+#                print "Val",val
+#                print "Val1",val1
+#                print "key",key
+#                print "key1",key1
+#    
+    
+    
+    
+    
+    ### Task 3: Create new feature(s)
+    ### Store to my_dataset for easy export below.
+   
+    
+    
+    
+    
+    
     
 
-max_bonus = 0
-data = featureFormat(data_dict, features)  
-for point in data:
-    salary = point[0]
-    bonus = point[1]
-    #if bonus != 97343619.0:
-        #matplotlib.pyplot.scatter( salary, bonus )
-    if bonus > max_bonus:
-        max_bonus = bonus
-#data frame to search for max
-#print person_data.iloc[0]
-print "Maximum bonus",max_bonus
-name_outlier = person_data[person_data['bonus'] == max_bonus].index.tolist()[0]
-
-print "Person With Max bonus:",name_outlier
-
-print "Details of the person with max bonus", data_dict[name_outlier]
-
-print "Total rows:",len(data_dict)
-new_dataset = removekey(data_dict,name_outlier)
-
-print "Total rows after removal",len(new_dataset)
-
-#my_dataset = 
-
-
-
-
-
-
-#print max_bonus
-
-#print "Person Information with highest bonus and salary\n",person_data.loc[person_data['bonus'] == max_bonus]
-
-#Value with maximum bonus is actually total
-
-"""
-matplotlib.pyplot.xlabel("salary")
-matplotlib.pyplot.ylabel("bonus")
-matplotlib.pyplot.show() 
-"""
-#person_data.to_csv('data.csv')
-person_data.to_csv('data.csv')
-print "\nCSV File Created"
-
-
-name_outlier = []
-"""
-for key,val in data_dict.iteritems():
-    if ((val['bonus'] =="NaN") or (val['salary'] =="NaN")):
-        continue
-    if((val['bonus'] > 5000000) and (val['salary'] > 1000000)):
-        name_outlier.append(key)
-
-
-print "Information of people in outlier",name_outlier
-
-for name in name_outlier:
-    print person_data.loc[name]
-    print "\n\n"
-
-"""
-
-
-
-
-
-
-
-### Task 2: Remove outliers
-### Task 3: Create new feature(s)
-### Store to my_dataset for easy export below.
-my_dataset = data_dict
-
-### Extract features and labels from dataset for local testing
-data = featureFormat(my_dataset, features_list, sort_keys = True)
-labels, features = targetFeatureSplit(data)
-
-### Task 4: Try a varity of classifiers
-### Please name your classifier clf for easy export below.
-### Note that if you want to do PCA or other multi-stage operations,
-### you'll need to use Pipelines. For more info:
-### http://scikit-learn.org/stable/modules/pipeline.html
-
-# Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
-
-### Task 5: Tune your classifier to achieve better than .3 precision and recall 
-### using our testing script. Check the tester.py script in the final project
-### folder for details on the evaluation method, especially the test_classifier
-### function. Because of the small size of the dataset, the script uses
-### stratified shuffle split cross validation. For more info: 
-### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
-
-# Example starting point. Try investigating other evaluation techniques!
-from sklearn.cross_validation import train_test_split
-features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.3, random_state=42)
-
-### Task 6: Dump your classifier, dataset, and features_list so anyone can
-### check your results. You do not need to change anything below, but make sure
-### that the version of poi_id.py that you submit can be run on its own and
-### generates the necessary .pkl files for validating your results.
-
-dump_classifier_and_data(clf, my_dataset, features_list)
+    
+    
+    
+    ### Extract features and labels from dataset for local testing
+    data = featureFormat(my_dataset, features_list, sort_keys = True)
+    labels, features = targetFeatureSplit(data)
+    
+    ### Task 4: Try a varity of classifiers
+    ### Please name your classifier clf for easy export below.
+    ### Note that if you want to do PCA or other multi-stage operations,
+    ### you'll need to use Pipelines. For more info:
+    ### http://scikit-learn.org/stable/modules/pipeline.html
+    
+    # Provided to give you a starting point. Try a variety of classifiers.
+    from sklearn.naive_bayes import GaussianNB
+    clf = GaussianNB()
+    
+    ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
+    ### using our testing script. Check the tester.py script in the final project
+    ### folder for details on the evaluation method, especially the test_classifier
+    ### function. Because of the small size of the dataset, the script uses
+    ### stratified shuffle split cross validation. For more info: 
+    ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
+    
+    # Example starting point. Try investigating other evaluation techniques!
+    from sklearn.cross_validation import train_test_split
+    features_train, features_test, labels_train, labels_test = \
+        train_test_split(features, labels, test_size=0.3, random_state=42)
+    
+    ### Task 6: Dump your classifier, dataset, and features_list so anyone can
+    ### check your results. You do not need to change anything below, but make sure
+    ### that the version of poi_id.py that you submit can be run on its own and
+    ### generates the necessary .pkl files for validating your results.
+    
+    dump_classifier_and_data(clf, my_dataset, features_list)
